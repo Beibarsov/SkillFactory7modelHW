9c269b7eb8fe1f22ed367fba8d309a05b5e337ee
Vitalij Nikonov <BarsStudio@gmail.com>
2022-02-08 23:53:14 +0800

    Добавьте файлы проекта.

---
+++ SkillFactory7modelHW/Order.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Order<TDelivery, TStruct> where TDelivery : Delivery
+{
+    public TDelivery Delivery;
+
+    public int Number;
+
+    public string Description;
+
+    public void DisplayAddress()
+    {
+        Console.WriteLine(Delivery.Address);
+    }
+
+    public Product[] Products;
+
+    public Customer Customer;
+    // ... Другие поля
+}
