ee2564391485958bc3cac616a82a8d4a2e52be43
Beibarsov <BarsStudio@gmail.com>
2022-02-09 13:18:48 +0800

    commit1

--- SkillFactory7modelHW/Delivery.cs
+++ SkillFactory7modelHW/Delivery.cs
@@ -3,3 +3,19 @@ abstract class Delivery
 {
     public string Address;
 }
+
+class HomeDelivery : Delivery
+{
+    /* ... */
+}
+
+class PickPointDelivery : Delivery
+{
+    /* ... */
+}
+
+class ShopDelivery : Delivery
+{
+    /* ... */
+}
+
